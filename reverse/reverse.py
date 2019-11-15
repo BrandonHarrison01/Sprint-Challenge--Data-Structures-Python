8b3e7dcddb29aca5a425783c96d7157864190830
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-11-15 12:11:30 -0700

    reverse passes tests

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -28,10 +28,13 @@ class LinkedList:
     self.head = node
 
   def contains(self, value):
+
     if not self.head:
       return False
+
     # get a reference to the node we're currently at; update this as we traverse the list
     current = self.head
+
     # check to see if we're at a valid node 
     while current:
       # return True if the current value we're looking at matches our target value
@@ -39,9 +42,38 @@ class LinkedList:
         return True
       # update our current node to the current node's next node
       current = current.get_next()
+      
     # if we've gotten here, then the target node isn't in our list
     return False
 
   def reverse_list(self):
     # TO BE COMPLETED
-    pass
+
+    cur = self.head
+    print(cur, 'this is cur')
+    prev = None
+    post = None
+    while cur is not None:
+      post = cur.get_next()
+      cur.set_next(prev)
+      prev = cur
+      cur = post
+      self.head = prev
+
+    return cur
+
+root = LinkedList()
+
+def test():
+  root.add_to_head(1)
+  root.add_to_head(2)
+  root.add_to_head(3)
+  root.add_to_head(4)
+  root.add_to_head(5)
+  print(root.head.value)
+  root.reverse_list()
+  print(root.head.value)
+  print(root.head.get_next().value)
+  print(root.head.get_next().get_next().value)
+
+test()
