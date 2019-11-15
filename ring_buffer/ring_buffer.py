abb0c6d52ea9d922f5bed0b3084275c5046d9884
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-11-15 09:46:08 -0700

    ring buffer passes all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,43 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+
+    if self.capacity == self.current:
+      self.current = 0
+    
+    self.storage[self.current] = item
+    self.current += 1
+
+
+
+
 
   def get(self):
-    pass
+
+    res = []
+    
+    for i in self.storage:
+      if i:
+        res.append(i)
+
+    return res
+
+
+
+
+
+
+
+
+
+# buffer = RingBuffer(3)
+# def buffer_test():
+#   buffer.append('a')
+#   buffer.append('b')
+#   buffer.append('c')
+#   buffer.append('d')
+#   buffer.append('e')
+#   buffer.append('f')
+#   buffer.get()
+
+# print(buffer_test())
